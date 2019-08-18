f31f0b980ecd2ecce3ba518321f71d7a4bf914fd
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2019-08-18 18:59:58 -0500

    Add scripts

---
+++ CPUFriendFriend.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python
+import os, sys, plistlib, zipfile, tempfile, binascii, shutil
+from Scripts import *
+
+class CPUFF:
+    def __init__(self):
+        self.u = utils.Utils("CPUFriendFriend")
+        self.d = downloader.Downloader()
+        self.r = run.Run()
+        self.scripts = os.path.join(os.path.dirname(os.path.realpath(__file__)),"Scripts")
+        self.out     = os.path.join(os.path.dirname(os.path.realpath(__file__)),"Results")
+        self.plist = None
+        self.plist_data = None
+        self.rc_url = "https://raw.githubusercontent.com/acidanthera/CPUFriend/master/ResourceConverter/ResourceConverter.sh"
+        self.iasl_url = "https://bitbucket.org/RehabMan/acpica/downloads/iasl.zip"
+        self.iasl = self.check_iasl()
+        self.freq_path = "/System/Library/Extensions/IOPlatformPluginFamily.kext/Contents/PlugIns/X86PlatformPlugin.kext/Contents/Resources"
+        self.has_epp  = False
+        self.epp_find = "6570700000000000000000000000000000000000"
+        self.board = self._get_current_board()
+        self.smbios = self._get_current_smbios()
+        self.rc_path = self._check_rc(self.rc_url)
+
+    def check_iasl(self):
+        target = os.path.join(self.scripts, "iasl")
+        if not os.path.exists(target):
+            # Need to download
+            temp = tempfile.mkdtemp()
+            try:
+                self._download_and_extract(temp,self.iasl_url)
+            except:
+                print("An error occurred :(")
+            shutil.rmtree(temp, ignore_errors=True)
+        if os.path.exists(target):
+            return target
+        return None
+
+    def _download_and_extract(self, temp, url):
+        ztemp = tempfile.mkdtemp(dir=temp)
+        zfile = os.path.basename(url)
+        self.u.head("Downloading IASL")
+        print("")
+        print("Downloading {}".format(os.path.basename(url)))
+        self.d.stream_to_file(url, os.path.join(ztemp,zfile))
+        print("")
+        print(" - Extracting")
+        btemp = tempfile.mkdtemp(dir=temp)
+        # Extract with built-in tools \o/
+        with zipfile.ZipFile(os.path.join(ztemp,zfile)) as z:
+            z.extractall(os.path.join(temp,btemp))
+        script_dir = self.scripts
+        for x in os.listdir(os.path.join(temp,btemp)):
+            if "iasl" in x.lower():
+                # Found one
+                print(" - Found {}".format(x))
+                print("   - Chmod +x")
+                self.r.run({"args":["chmod","+x",os.path.join(btemp,x)]})
+                print("   - Copying to {} directory".format(os.path.basename(script_dir)))
+                shutil.copy(os.path.join(btemp,x), os.path.join(script_dir,x))
+
+    def _get_rc(self, url):
+        self.u.head("Downloading ResourceConverter")
+        print("")
+        target = os.path.join(self.scripts,os.path.basename(url))
+        print("Downloading {} from:\n{}".format(os.path.basename(url),url))
+        return self.d.stream_to_file(url, target)
+
+    def _check_rc(self, url):
+        target = os.path.join(self.scripts,os.path.basename(url))
+        if os.path.exists(target):
+            return target
+        return self._get_rc(url)
+
+    def _decode(self, var):
+        if sys.version_info >= (3,0) and isinstance(var, bytes):
+            var = var.decode("utf-8","ignore")
+        return var
+
+    def _get_value(self,value):
+        out = self._decode(self.r.run({"args":["ioreg","-p","IODeviceTree","-d","2","-k",value]})[0])
+        v = "Unknown"
+        try:
+            v = out.split('{}" = '.format(value))[1].split('<"')[1].split('">')[0]
+        except:
+            pass
+        return v
+
+    def _get_current_board(self):
+        return self._get_value("board-id")
+
+    def _get_current_smbios(self):
+        return self._get_value("product-name")
+
+    def _get_epp_desc(self,epp):
+        epp_int = epp if isinstance(epp,int) else int(epp,16)
+        epp_desc = "Unknown"
+        if epp_int < 64:
+            epp_desc = "Performance"
+        elif epp_int < 128:
+            epp_desc = "Balanced Performance"
+        elif epp_int < 192:
+            epp_desc = "Balanced Power"
+        else:
+            epp_desc = "Power Saving"
+        return epp_desc
+
+    def _get_freq_info(self,x):
+        freq = epp = None
+        data = x if not isinstance(x,plistlib.Data) else x.data
+        str_data = self._decode(binascii.hexlify(data)).upper()
+        freq = str_data[8:10]
+        if self.epp_find in str_data:
+            epp = str_data.split(self.epp_find)[1][:2]
+        return (freq,epp)
+
+    def _display_desc(self,desc):
+        self.u.head()
+        print("")
+        print("Current Board:  {}".format(self.board))
+        print("Current SMBIOS: {}".format(self.smbios))
+        print("")
+        for i,x in enumerate(desc):
+            print(" {}. {}00MHz --> {}00MHz".format(i+1,int(x["start_freq"],16),int(x["end_freq"],16)))
+            if "start_epp" in x:
+                print("  -->{} ({}) --> {} ({})".format(
+                    x["start_epp"],
+                    self._get_epp_desc(x["start_epp"]),
+                    x["end_epp"],
+                    self._get_epp_desc(x["end_epp"])
+                ))
+        if len(desc):
+            print("")
+
+    def main(self):
+        if self.board.lower() == "unknown":
+            self.u.head("CPUFriendFriend")
+            print("")
+            print("Couldn't determine board id!")
+            print("Aborting!\n")
+            exit(1)
+        if not self.plist:
+            self.plist = os.path.join(self.freq_path,self.board+".plist")
+            try:
+                with open(self.plist,"rb") as f:
+                    self.plist_data = plist.load(f)
+            except Exception as e:
+                self.u.head("CPUFriendFriend")
+                print("")
+                print("Could not load {}!\nAborting!\n".format(self.board+".plist"))
+                print(e)
+                print("")
+                exit(1)
+        if self.plist_data.get("IOPlatformPowerProfile",{}).get("FrequencyVectors",None) == None:
+            self.u.head("CPUFriendFriend")
+            print("")
+            print("FrequencyVectors not found in {}!\nAborting!\n".format(self.board+".plist"))
+            exit(1)
+        new_freq = []
+        new_desc = []
+        total = len(self.plist_data.get("IOPlatformPowerProfile",{}).get("FrequencyVectors",[]))
+        for i,x in enumerate(self.plist_data.get("IOPlatformPowerProfile",{}).get("FrequencyVectors",[])):
+            freq,epp = self._get_freq_info(x)
+            data = x if not isinstance(x,plistlib.Data) else x.data
+            str_data = self._decode(binascii.hexlify(data)).upper()
+            curr_desc = {"start_freq":freq}
+            while True:
+                self._display_desc(new_desc)
+                # Display the hex, ask for a new value
+                print("Current Frequency Vector:\n")
+                print("{} of {}: {} ({}00 MHz)\n".format(i+1,total,freq,int(freq,16)))
+                new = self.u.grab("Enter the new min hex freq (800Mhz would be 08, 1300Mhz would be 0C):  ").upper()
+                if new == "Q":
+                    exit()
+                new = new.replace("0X","")
+                new = "".join([x for x in new if x in "0123456789ABCDEF"])
+                if len(new) != 2:
+                    continue
+                # Save the changes
+                str_data = str_data[:8]+new+str_data[10:]
+                curr_desc["end_freq"] = new
+                break
+            if epp:
+                self._display_desc(new_desc)
+                print("EPP Range:")
+                print("  0x00-0x3F    :    Performance")
+                print("  0x40-0x7F    :    Balance performance")
+                print("  0x80-0xBF    :    Balance power")
+                print("  0xC0-0xFF    :    Power")
+                print("")
+                curr_desc["start_epp"] = epp
+                while True:
+                    # Display the hex, ask for a new value
+                    print("{} of {}: EPP {} ({})\n".format(i+1,total,epp,self._get_epp_desc(epp)))
+                    new = self.u.grab("Enter the new EPP value in hex:  ").upper()
+                    if new == "Q":
+                        exit()
+                    new = new.replace("0X","")
+                    new = "".join([x for x in new if x in "0123456789ABCDEF"])
+                    if len(new) != 2:
+                        continue
+                    # Save the changes
+                    ind = str_data.find(self.epp_find)
+                    str_data = str_data[:ind+len(self.epp_find)]+new+str_data[ind+len(self.epp_find)+2:]
+                    curr_desc["end_epp"] = new
+                    break
+            new_desc.append(curr_desc)
+            # Got the new data - convert it and store it
+            if sys.version_info < (3, 0):
+                new_freq.append(plistlib.Data(binascii.unhexlify(str_data)))
+            else:
+                new_freq.append(binascii.unhexlify(str_data.encode("utf-8")))
+        # Save the changes
+        self._display_desc(new_desc)
+        print("Saving to {}...".format(self.board+".plist"))
+        self.plist_data["IOPlatformPowerProfile"]["FrequencyVectors"] = new_freq
+        if os.path.exists(self.out):
+            print("Found prior Results - removing...")
+            shutil.rmtree(self.out,ignore_errors=True)
+        os.makedirs(self.out)
+        target_plist = os.path.join(self.out,self.board+".plist")
+        with open(target_plist,"wb") as f:
+            plist.dump(self.plist_data,f)
+        # Run the script if found
+        if self.rc_path and os.path.exists(self.rc_path):
+            print("Running {}...".format(os.path.basename(self.rc_path)))
+            cwd = os.getcwd()
+            os.chdir(self.out)
+            out = self.r.run({"args":["bash",self.rc_path,"-a",target_plist]})
+            if out[2] != 0:
+                print(out[1])
+            out = self.r.run({"args":["bash",self.rc_path,"-k",target_plist]})
+            if out[2] != 0:
+                print(out[1])
+            if self.iasl:
+                print("Compiling SSDTs...")
+                for x in os.listdir(self.out):
+                    if x.startswith(".") or not x.lower().endswith(".dsl"):
+                        continue
+                    print(" - {}".format(x))
+                    out = self.r.run({"args":[self.iasl,x]})
+                    if out[2] != 0:
+                        print(out[1])
+            os.chdir(cwd)
+            self.r.run({"args":["open",self.out]})
+        print("")
+        print("Done.")
+        exit()
+
+c = CPUFF()
+c.main()
